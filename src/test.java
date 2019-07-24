d133ba10071eb0c8df0492d75921e4f2ab0f60e6
Augustvic <18716558336@163.com>
2019-07-24 16:13:19 +0800

    2019/7/24

--- src/test.java
+++ src/test.java
@@ -3,33 +3,20 @@ import java.util.*;
 public class test {
 
     public static void main(String[] args) {
-        int[][] nums = {{1,  5,  9}, {10, 11, 13}, {12, 13, 15}};
-        System.out.println(t.kthSmallest(nums, 8));
+        System.out.println(Arrays.toString(t.constructArray(10, 9)));
     }
 
     public static test t = new test();
 
-    public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> ret = new LinkedList<>();
-        if (nums == null || nums.length == 0)
-            return ret;
-        int index = 0;
-        while (index < nums.length) {
-            if (nums[index] != index + 1 && nums[nums[index] - 1] != nums[index]) {
-                swap(nums, index, nums[index] - 1);
-            }
-            else {
-                if (!ret.contains(nums[index]))
-                    ret.add(nums[index]);
-                index++;
-            }
+    public int[] constructArray(int n, int k) {
+        int[] ret = new int[n];
+        ret[0] = 1;
+        for (int i = 1, interval = k; i <= k; i++, interval--) {
+            ret[i] = (i % 2 == 1 ? ret[i - 1] + interval : ret[i - 1] - interval);
+        }
+        for (int i = k + 1; i < n; i++) {
+            ret[i] = i + 1;
         }
         return ret;
-    }
-
-    public void swap(int[] nums, int a, int b) {
-        int t = nums[a];
-        nums[a] = nums[b];
-        nums[b] = t;
     }
 }
