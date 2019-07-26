b7860909f43c476b84939e87adbab675ebeaa8bc
August <18716558336@163.com>
2019-07-26 11:34:19 +0800

    2019/7/26

--- src/test.java
+++ src/test.java
@@ -3,23 +3,13 @@ import java.util.*;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println(t.maxChunksToSorted(new int[]{1, 2, 0, 3}));
-        ArrayList<Integer> l = new ArrayList<>();
+        System.out.println(t.hammingDistance(1, 4));
     }
 
     public static test t = new test();
 
-    public int maxChunksToSorted(int[] arr) {
-        if (arr == null || arr.length == 0)
-            return 0;
-        int ret = 0;
-        int right = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            //right代表到目前为止的最大值
-            right = Math.max(arr[i], right);
-            if(right == i)
-                ret++;
-        }
-        return ret;
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        return Integer.bitCount(z);
     }
 }
