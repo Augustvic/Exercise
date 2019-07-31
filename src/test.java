cf9c1c129a610ea62ccde99747499964ea4e276d
August <18716558336@163.com>
2019-07-31 19:24:27 +0800

    test

--- src/test.java
+++ src/test.java
@@ -3,6 +3,15 @@ import java.util.*;
 public class test {
 
     public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        Iterator<Integer> it = l.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+            it.remove();
+        }
         System.out.println(t.hammingDistance(1, 4));
     }
 
