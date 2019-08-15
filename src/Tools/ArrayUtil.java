c4e7a76f40d3d5c2c6cea130e1df9c23f2183d5e
Augustvic <18716558336@163.com>
2019-08-15 11:33:41 +0800

    2019/8/15

--- src/Tools/ArrayUtil.java
+++ src/Tools/ArrayUtil.java
@@ -2,11 +2,11 @@ package Tools;
 
 public class ArrayUtil {
 
-    public static boolean isEmpty(int[] nums) {
+    public static <T> boolean isEmpty(T[] nums) {
         return (nums == null) || (nums.length == 0);
     }
 
-    public static boolean isEmpty(int[][] matrix) {
+    public static <T> boolean isEmpty(T[][] matrix) {
         return (matrix == null || matrix.length == 0 || matrix[0].length == 0);
     }
 }
