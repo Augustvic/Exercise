6ab370046ba701b5d4eed6b48538bc403ac6cf29
August <18716558336@163.com>
2019-09-11 21:02:09 +0800

    2019/9/11

--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
+import DataStructure.BinaryTree;
 import DataStructure.BinaryTreeNode;
 import DataStructure.ComplexListNode;
 import DataStructure.ListNode;
@@ -9,8 +10,46 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
+        BinaryTree test = new BinaryTree();
+        System.out.println(t.getDepth(test.full_binary_tree()));
+        System.out.println(t.getDepth(test.complete_binary_tree()));
+        System.out.println(t.getDepth(test.one_node_binary_tree()));
+        System.out.println(t.getDepth(test.left_node_only_binary_tree()));
+        System.out.println(t.getDepth(test.right_node_only_binary_tree()));
+        System.out.println(t.isBalancedOptimal(test.full_binary_tree()));
+        System.out.println(t.isBalancedOptimal(test.complete_binary_tree()));
+        System.out.println(t.isBalancedOptimal(test.one_node_binary_tree()));
+        System.out.println(t.isBalancedOptimal(test.left_node_only_binary_tree()));
+        System.out.println(t.isBalancedOptimal(test.right_node_only_binary_tree()));
     }
 
     public static test t = new test();
+
+    public int getDepth(BinaryTreeNode root) {
+        if (root == null)
+            return 0;
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    private boolean flag = true;
+
+    public boolean isBalancedOptimal(BinaryTreeNode root) {
+        if (root == null)
+            return true;
+        isBalancedWithDepth(root);
+        return flag;
+    }
+
+    private int isBalancedWithDepth(BinaryTreeNode root) {
+        if (root == null)
+            return 0;
+        int left = isBalancedWithDepth(root.left);
+        int right = isBalancedWithDepth(root.right);
+        if (Math.abs(left - right) > 1) {
+            flag = false;
+        }
+        return Math.max(left, right) + 1;
     }
 }
