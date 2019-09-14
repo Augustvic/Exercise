0879a450b757307070382b86aa900b51748fd86c
August <18716558336@163.com>
2019-09-14 10:35:50 +0800

    2019/9/14

--- src/test.java
+++ src/test.java
@@ -10,46 +10,64 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
-        BinaryTree test = new BinaryTree();
-        System.out.println(t.getDepth(test.full_binary_tree()));
-        System.out.println(t.getDepth(test.complete_binary_tree()));
-        System.out.println(t.getDepth(test.one_node_binary_tree()));
-        System.out.println(t.getDepth(test.left_node_only_binary_tree()));
-        System.out.println(t.getDepth(test.right_node_only_binary_tree()));
-        System.out.println(t.isBalancedOptimal(test.full_binary_tree()));
-        System.out.println(t.isBalancedOptimal(test.complete_binary_tree()));
-        System.out.println(t.isBalancedOptimal(test.one_node_binary_tree()));
-        System.out.println(t.isBalancedOptimal(test.left_node_only_binary_tree()));
-        System.out.println(t.isBalancedOptimal(test.right_node_only_binary_tree()));
+        int[] nums1 = {2, 4, 3, 3, 2, 5, 5, 6};
+        t.find1(nums1);
+        int[] nums3 = {2, 4, 3, 2, 4, 7, 3, 3, 4, 2};
+        System.out.println(t.find2(nums3));
     }
 
     public static test t = new test();
 
-    public int getDepth(BinaryTreeNode root) {
-        if (root == null)
-            return 0;
-        int leftDepth = getDepth(root.left);
-        int rightDepth = getDepth(root.right);
-        return Math.max(leftDepth, rightDepth) + 1;
-    }
-
-    private boolean flag = true;
-
-    public boolean isBalancedOptimal(BinaryTreeNode root) {
-        if (root == null)
-            return true;
-        isBalancedWithDepth(root);
-        return flag;
-    }
-
-    private int isBalancedWithDepth(BinaryTreeNode root) {
-        if (root == null)
-            return 0;
-        int left = isBalancedWithDepth(root.left);
-        int right = isBalancedWithDepth(root.right);
-        if (Math.abs(left - right) > 1) {
-            flag = false;
+    public void find1(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+        int xorAll = 0;
+        for (int num : nums) {
+            xorAll = xorAll ^ num;
         }
-        return Math.max(left, right) + 1;
+        int index = indexOfLast1(xorAll);
+        int a = 0;
+        int b = 0;
+        for (int num : nums) {
+            if (is1(num, index))
+                a = a ^ num;
+            else
+                b = b ^ num;
+        }
+        System.out.println("[" + a + "," + b + "]");
+    }
+
+    private int indexOfLast1(int k) {
+        int index = 0;
+        while ((k & 1) == 0) {
+            index++;
+            k = k >> 1;
+        }
+        return index;
+    }
+
+    private boolean is1(int k, int index) {
+        k = k >> index;
+        return (k & 1) == 1;
+    }
+
+    public int find2(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        int[] bits = new int[32];
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                sum += (nums[j] % 2 == 0) ? 0 : 1;
+                nums[j] = nums[j] >> 1;
+            }
+            bits[i] = sum % 3;
+        }
+        int ret = 0;
+        for (int i = 31; i >= 0; i--) {
+            ret = ret << 1;
+            ret += bits[i];
+        }
+        return ret;
     }
 }
