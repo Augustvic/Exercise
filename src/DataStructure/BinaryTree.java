4eb25dff70494a6a2200ce15af40c32b1c373452
August <18716558336@163.com>
2019-09-26 10:06:19 +0800

    update

--- src/DataStructure/BinaryTree.java
+++ src/DataStructure/BinaryTree.java
@@ -4,7 +4,7 @@ import DataStructure.BinaryTreeNode;
 
 public class BinaryTree {
     // 完全二叉树
-    public BinaryTreeNode complete_binary_tree() {
+    public static BinaryTreeNode complete_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.left = new BinaryTreeNode(2);
         root.right = new BinaryTreeNode(3);
@@ -14,7 +14,7 @@ public class BinaryTree {
         return root;
     }
     //满二叉树
-    public BinaryTreeNode full_binary_tree() {
+    public static BinaryTreeNode full_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.left = new BinaryTreeNode(2);
         root.right = new BinaryTreeNode(3);
@@ -25,12 +25,12 @@ public class BinaryTree {
         return root;
     }
     //只有一个节点的二叉树
-    public BinaryTreeNode one_node_binary_tree() {
+    public static BinaryTreeNode one_node_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         return root;
     }
     //所有节点都只有左子节点的二叉树
-    public BinaryTreeNode left_node_only_binary_tree() {
+    public static BinaryTreeNode left_node_only_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.left = new BinaryTreeNode(2);
         root.left.left = new BinaryTreeNode(3);
@@ -38,7 +38,7 @@ public class BinaryTree {
         return root;
     }
     //所有节点都只有右子节点的二叉树
-    public BinaryTreeNode right_node_only_binary_tree() {
+    public static BinaryTreeNode right_node_only_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.right = new BinaryTreeNode(2);
         root.right.right = new BinaryTreeNode(3);
@@ -46,7 +46,7 @@ public class BinaryTree {
         return root;
     }
     //所有元素相等的二叉树
-    public BinaryTreeNode equal_node_binary_tree() {
+    public static BinaryTreeNode equal_node_binary_tree() {
         BinaryTreeNode root = new BinaryTreeNode(16);
         root.left = new BinaryTreeNode(16);
         root.right = new BinaryTreeNode(16);
@@ -57,7 +57,7 @@ public class BinaryTree {
     }
 
     // 完全二叉检索树
-    public BinaryTreeNode complete_binary_search_tree() {
+    public static BinaryTreeNode complete_binary_search_tree() {
         BinaryTreeNode root = new BinaryTreeNode(5);
         root.left = new BinaryTreeNode(3);
         root.right = new BinaryTreeNode(7);
@@ -67,7 +67,7 @@ public class BinaryTree {
         return root;
     }
     //满二叉检索树
-    public BinaryTreeNode full_binary_search_tree() {
+    public static BinaryTreeNode full_binary_search_tree() {
         BinaryTreeNode root = new BinaryTreeNode(5);
         root.left = new BinaryTreeNode(3);
         root.right = new BinaryTreeNode(7);
@@ -78,12 +78,12 @@ public class BinaryTree {
         return root;
     }
     //只有一个节点的二叉检索树
-    public BinaryTreeNode one_node_binary_search_tree() {
+    public static BinaryTreeNode one_node_binary_search_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         return root;
     }
     //所有节点都只有左子节点的二叉检索树
-    public BinaryTreeNode left_node_only_binary_search_tree() {
+    public static BinaryTreeNode left_node_only_binary_search_tree() {
         BinaryTreeNode root = new BinaryTreeNode(4);
         root.left = new BinaryTreeNode(3);
         root.left.left = new BinaryTreeNode(2);
@@ -91,7 +91,7 @@ public class BinaryTree {
         return root;
     }
     //所有节点都只有右子节点的二叉检索树
-    public BinaryTreeNode right_node_only_binary_search_tree() {
+    public static BinaryTreeNode right_node_only_binary_search_tree() {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.right = new BinaryTreeNode(2);
         root.right.right = new BinaryTreeNode(3);
--- src/test.java
+++ src/test.java
@@ -7,32 +7,57 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
-        int[] nums = {2, 0, 1};
-        t.sortColors(nums);
-        System.out.println();
+
+        // 栈：先进后出，后进先出(LIFO)
+        Stack<Integer> iStack = new Stack<Integer>(); //实例化一个存储Integer类型对象的栈对象
+
+        iStack.push(0); //入栈
+        iStack.push(1);
+
+        for (int i = 0; i < 5; i++) {
+
+            int f2 = iStack.pop(); //出栈
+            int f1 = iStack.pop();
+
+            int tmp = f1 + f2;
+
+            iStack.push(f1);
+            iStack.push(f2);
+            iStack.push(tmp);
+
+        }
+
+        while (!iStack.empty()) { //判断栈是否为空
+            System.out.println(iStack); //打印栈里面的数据
+            iStack.pop();
+        }
     }
 
     public static test t = new test();
 
-    public void sortColors(int[] nums) {
-        if (nums == null || nums.length == 0)
+    public void postOrder(BinaryTreeNode root) {
+        if (root == null)
             return;
-        int l = 0;
-        int r = nums.length - 1;
-        int curr = 0;
-        while (curr <= r) {
-            if (nums[curr] == 0)
-                swap(nums, l++, curr++);
-            else if (nums[curr] == 2)
-                swap(nums, curr, r--);
-            else
-                curr++;
+        Stack<BinaryTreeNode> st1 = new Stack<>();
+        Stack<BinaryTreeNode> st2 = new Stack<>();
+        while (root != null || !st1.isEmpty()) {
+            while (root != null) {
+                st1.push(root);
+                st2.push(root);
+                root = root.right;
+            }
+            if (!st1.isEmpty()) {
+                root = st1.pop();
+                root = root.left;
+            }
+        }
+        while (!st2.isEmpty()) {
+            root = st2.pop();
+            visit(root);
         }
     }
 
-    private void swap(int[] nums, int l, int r) {
-        int t = nums[l];
-        nums[l] = nums[r];
-        nums[r] = t;
+    private void visit(BinaryTreeNode root) {
+        System.out.print(root.val + " ");
     }
 }
