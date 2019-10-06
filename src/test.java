0b4639137aef9bc7682587067bb69d82e53957ee
August <18716558336@163.com>
2019-10-06 20:06:52 +0800

    2019/10/6

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 import DataStructure.*;
 import Tools.*;
+import javafx.util.Pair;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -7,22 +8,33 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
-        int[] nums = {3,4,5,1,2};
-        System.out.println(t.findMin(nums));
+        int[][] M = {{1,1,0},
+                {1,1,0},
+                {0,0,1}};
+        System.out.println(t.findCircleNum(M));
     }
 
     public static test t = new test();
 
-    public int findMin(int[] nums) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > nums[r])
-                l = mid + 1;
-            else
-                r = mid;
+    public int findCircleNum(int[][] M) {
+        if (M == null || M.length == 0 || M[0].length == 0)
+            return 0;
+        int cnt = 0;
+        boolean[] visited = new boolean[M.length];
+        for (int i = 0; i < M.length; i++) {
+            if (!visited[i]) {
+                cnt++;
+                dfs(M, i, visited);
+            }
         }
-        return l;
+        return cnt;
+    }
+
+    private void dfs(int[][] M, int i, boolean[] visited) {
+        visited[i] = true;
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == '1' && !visited[j])
+                dfs(M, j, visited);
+        }
     }
 }
