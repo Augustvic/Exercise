90eaef0704bd105da7d6c9cbba8d2f8a66cbb840
August <18716558336@163.com>
2019-10-19 11:49:36 +0800

    2019/10/19

--- src/test.java
+++ src/test.java
@@ -8,54 +8,21 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
+        System.out.println(t.minSteps(4));
     }
 
     public static test t = new test();
 
-    private boolean[][] rowUsed = new boolean[9][10];
-    private boolean[][] columnUsed = new boolean[9][10];
-    private boolean[][] cubeUsed = new boolean[9][10];
-
-    public int solveSudoku(int W, int N, int[] weights, int[] values) {
-        int[][] dp = new int[N + 1][W + 1];
-        for (int i = 1; i <= N; i++) {
-            int w = weights[i - 1];
-            int v = values[i - 1];
-            for (int j = 1; j <= W; j++) {
-                if (j >= w)
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
-                else
-                    dp[i][j] = dp[i - 1][j];
+    public int minSteps(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0)
+                    dp[i] = Math.min(dp[i], dp[j] + i / j);
             }
         }
-        return dp[N][W];
-    }
-
-    private boolean solveSudokuHelp(char[][] board, int i, int j) {
-        while (i < 9 && board[i][j] != '.') {
-            i = (j == 8) ? (i + 1) : i;
-            j = (j == 8) ? 0 : j + 1;
-        }
-        for (int k = 1; k <= 9; k++) {
-            if (!rowUsed[i][k] && !columnUsed[j][k] && !cubeUsed[cubeNum(i, j)][k]) {
-                board[i][j] = (char)(k + '0');
-                rowUsed[i][k] = true;
-                columnUsed[j][k] = true;
-                cubeUsed[cubeNum(i, j)][k] = true;
-                if (solveSudokuHelp(board, i, j))
-                    return true;
-                board[i][j] = '.';
-                rowUsed[i][k] = false;
-                columnUsed[j][k] = false;
-                cubeUsed[cubeNum(i, j)][k] = false;
-            }
-        }
-        return false;
-    }
-
-    private int cubeNum(int i, int j) {
-        i = i / 3;
-        j = j / 3;
-        return i * 3 + j;
+        return dp[n];
     }
 }
