5a6f3774d47678be60b566d1b95ad0280572902b
August <18716558336@163.com>
2019-10-23 19:47:07 +0800

    2019/10/23

--- src/test.java
+++ src/test.java
@@ -8,78 +8,40 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
 
     public static void main(String[] args) {
-//        t.printOneToMaxOfNDigits(3);
-        System.out.println(t.add("123", "456"));
-        System.out.println(t.add("123", "0"));
-        System.out.println(t.add("123", "1234"));
-        System.out.println(t.add("0", "0"));
+        System.out.println(t.isNumeric("123.45e+6"));
     }
 
     public static test t = new test();
 
-    public void printOneToMaxOfNDigits(int n) {
-        if (n <= 0)
-            return;
-        StringBuilder sb = new StringBuilder();
-        sb.setLength(n);
-        printHelp(n, 0, sb);
-    }
+    private int pos = 0;
 
-    private void printHelp(int n, int pos, StringBuilder sb) {
-        if (n == pos) {
-            printString(sb.toString());
-            return;
-        }
-        for (int i = 0; i < 10; i++) {
-            sb.setCharAt(pos, (char)(i + '0'));
-            printHelp(n, pos + 1, sb);
-        }
-    }
-
-    private void printString(String s) {
-        int start = 0;
-        while (start < s.length() && s.charAt(start) == '0') start++;
-        System.out.print(s.substring(start));
-        System.out.println();
-    }
-
-    public String add(String s1, String s2) {
-        StringBuilder sb1 = new StringBuilder(s1);
-        StringBuilder sb2 = new StringBuilder(s2);
-        reverse(sb1);
-        reverse(sb2);
-        StringBuilder ret = new StringBuilder();
-        int carry = 0;
-        int sum = 0;
-        int pos = 0;
-        while (pos < sb1.length() && pos < sb2.length()) {
-            int a = sb1.charAt(pos) - '0';
-            int b = sb2.charAt(pos) - '0';
-            sum = (a + b + carry) % 10;
-            carry = (a + b + carry) / 10;
-            ret.append((char)(sum + '0'));
+    public boolean isNumeric(String s) {
+        pos = 0;
+        if (s == null || s.length() == 0)
+            return false;
+        boolean ret = scanInteger(s);
+        if (pos < s.length() && s.charAt(pos) == '.') {
             pos++;
+            ret = scanUnsignedInteger(s) || ret;
         }
-        if (pos < sb1.length())
-            ret.append(sb1.substring(pos));
-        if (pos < sb2.length())
-            ret.append(sb2.substring(pos));
-        if (carry != 0)
-            ret.append((char)(carry + '0'));
-        reverse(ret);
-        return ret.toString();
+        if (pos < s.length() && (s.charAt(pos) == 'e' || s.charAt(pos) == 'E')) {
+            pos++;
+            ret = ret && scanInteger(s);
+        }
+        return ret && (pos == s.length());
     }
 
-    private void reverse(StringBuilder sb) {
-        int l = 0;
-        int r = sb.length() - 1;
-        while (l < r) {
-            char c = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, c);
-            l++;
-            r--;
-        }
+    private boolean scanInteger(String s) {
+        if (pos < s.length() && (s.charAt(pos) == '+' || s.charAt(pos) == '-'))
+            pos++;
+        return scanUnsignedInteger(s);
+    }
+
+    private boolean scanUnsignedInteger(String s) {
+        int start = pos;
+        while (pos < s.length() && s.charAt(pos) >= '0' && s.charAt(pos) <= '9')
+            pos++;
+        return pos > start;
     }
 
 }
