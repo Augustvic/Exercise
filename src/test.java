987baf8eac8974fb9753070ef4fa33ddd888416c
August <18716558336@163.com>
2020-01-08 22:10:17 +0800

    2020/1/8

--- src/test.java
+++ src/test.java
@@ -6,36 +6,57 @@ import javafx.util.Pair;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class test {
     public static void main(String[] args) {
-        t.print(BinaryTree.complete_binary_tree());
+        int[] input = {4,5,1,6,2,7,3,8};
+        t.GetLeastNumbers_Solution(input, 4);
     }
 
-    public static test t = new test();
+    private static test t = new test();
 
-    public void print(BinaryTreeNode root) {
-        ArrayDeque<BinaryTreeNode> aq = new ArrayDeque<>();
-        aq.offer(root);
-        int currLevel = 1;
-        int nextLevel = 0;
-        while (!aq.isEmpty()) {
-            if (currLevel == 0) {
-                System.out.println();
-                currLevel = nextLevel;
-                nextLevel = 0;
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        int l = 0;
+        int r = input.length - 1;
+        ArrayList<Integer> ret = new ArrayList<>();
+        while (l <= r) {
+            int index = partition(input, l, r);
+            if (index == k - 1 && copy(ret, input, k)) {
+                return ret;
             }
-            BinaryTreeNode tn = aq.poll();
-            System.out.print(tn.val + " ");
-            currLevel--;
-            if (tn.left != null) {
-                aq.offer(tn.left);
-                nextLevel++;
+            else if (index > k - 1) {
+                r = index - 1;
             }
-            if (tn.right != null) {
-                aq.offer(tn.right);
-                nextLevel++;
+            else {
+                l = index + 1;
             }
         }
+        return ret;
     }
+
+    private int partition(int[] input, int l, int r) {
+        int pivot = r;
+        while (l < r) {
+            while (l < r && input[l] < input[pivot]) l++;
+            while (l < r && input[r] >= input[pivot]) r--;
+            swap(input, l, r);
+        }
+        swap(input, l, pivot);
+        return l;
+    }
+
+    private void swap(int[] input, int l, int r) {
+        int t = input[l];
+        input[l] = input[r];
+        input[r] = t;
+    }
+
+    private boolean copy(ArrayList<Integer> ret, int[] input, int end) {
+        for (int i = 0; i < end; i++) {
+            ret.add(input[i]);
+        }
+        return true;
+    }
+
 }
