ae83837fdfad6fed4fba1741dd91ce8951515f33
gongchunxia.gc <gongchunxia.gc@alibaba-inc.com>
2021-08-07 12:02:03 +0800

    20210807

--- src/Main.java
+++ src/Main.java
@@ -3,73 +3,24 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {2, 2, 2, 2, 2};
-        int target = 8;
-        System.out.println(fourSum(nums, target));
+        int n = 4;
+        System.out.println(countAndSay(n));
     }
 
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ret = new ArrayList<>();
-        if (nums.length < 4) {
-            return ret;
-        }
-        Arrays.sort(nums);
-        int max = nums[nums.length - 1];
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] + max * 3 < target) {
-                continue;
-            }
-            if (nums[i] * 4 > target) {
-                continue;
-            }
-            threeSum(ret, nums, target - nums[i], i + 1, i);
-        }
-        return ret;
-    }
-
-    public static void threeSum(List<List<Integer>> ret, int[] nums, int target, int start, int a) {
-        int max = nums[nums.length - 1];
-        for (int i = start; i < nums.length; i++) {
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] + max * 2 < target) {
-                continue;
-            }
-            if (nums[i] * 3 > target) {
-                continue;
-            }
-            twoSum(ret, nums, target - nums[i], i + 1, a, i);
-        }
-    }
-
-    public static void twoSum(List<List<Integer>> ret, int[] nums, int target, int start, int a, int b) {
-        int l = start, r = nums.length - 1;
-        while (l < r) {
-            int sum = nums[l] + nums[r];
-            if (sum == target) {
-                List<Integer> list = new ArrayList<>();
-                list.add(nums[a]);
-                list.add(nums[b]);
-                list.add(nums[l]);
-                list.add(nums[r]);
-                ret.add(list);
-                while (l < r && nums[l] == nums[l + 1]) {
-                    l++;
+    public static String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder("1");
+        for (int i = 2; i <= n; i++) {
+            StringBuilder newSb = new StringBuilder();
+            for (int m = 0, k; m < sb.length(); m = k) {
+                k = m + 1;
+                while (k < sb.length() && sb.charAt(k) == sb.charAt(m)) {
+                    k++;
                 }
-                while (l < r && nums[r] == nums[r - 1]) {
-                    r--;
-                }
-                l++;
-                r--;
-            } else if (sum > target) {
-                r--;
-            } else {
-                l++;
+                newSb.append(k - m);
+                newSb.append(sb.charAt(m));
             }
+            sb = newSb;
         }
+        return sb.toString();
     }
 }
