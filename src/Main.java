ded19c4e4db4874fc559b073584445814cbc0d83
gongchunxia.gc <gongchunxia.gc@alibaba-inc.com>
2021-08-08 17:31:01 +0800

    20210808

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,59 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 4;
-        System.out.println(countAndSay(n));
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(multiply(num1, num2));
     }
 
-    public static String countAndSay(int n) {
-        StringBuilder sb = new StringBuilder("1");
-        for (int i = 2; i <= n; i++) {
-            StringBuilder newSb = new StringBuilder();
-            for (int m = 0, k; m < sb.length(); m = k) {
-                k = m + 1;
-                while (k < sb.length() && sb.charAt(k) == sb.charAt(m)) {
-                    k++;
-                }
-                newSb.append(k - m);
-                newSb.append(sb.charAt(m));
+    public static String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        String s1 = reverse(num1);
+        String s2 = reverse(num2);
+        int[] ret = new int[s1.length() + s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            int carry = 0;
+            for (int j = 0; j < s2.length(); j++) {
+                int pos = i + j;
+                int a = s1.charAt(i) - '0';
+                int b = s2.charAt(j) - '0';
+                int sum = ret[pos] + a * b + carry;
+                ret[pos] = sum % 10;
+                carry = sum / 10;
             }
-            sb = newSb;
+            if (carry != 0) {
+                ret[i + s2.length()] = carry;
+            }
+        }
+        return reverse(ret);
+    }
+
+    private static String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    private static String reverse(int[] arr) {
+        int end = arr.length - 1;
+        while (end >= 0 && arr[end] == 0) {
+            end--;
+        }
+        int l = 0, r = end;
+        while (l < r) {
+            int t = arr[l];
+            arr[l] = arr[r];
+            arr[r] = t;
+            l++;
+            r--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= end; i++) {
+            sb.append((char)(arr[i] + '0'));
         }
         return sb.toString();
     }
