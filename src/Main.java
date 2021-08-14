645c8e939f1decfe716ce100dd2b572d2e403e7b
gongchunxia.gc <gongchunxia.gc@alibaba-inc.com>
2021-08-14 21:21:02 +0800

    20210814

--- src/Main.java
+++ src/Main.java
@@ -3,60 +3,46 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String num1 = "123";
-        String num2 = "456";
-        System.out.println(multiply(num1, num2));
+        System.out.println(myPow(2, Integer.MIN_VALUE));
     }
 
-    public static String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) {
-            return "0";
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0d;
         }
-        String s1 = reverse(num1);
-        String s2 = reverse(num2);
-        int[] ret = new int[s1.length() + s2.length()];
-        for (int i = 0; i < s1.length(); i++) {
-            int carry = 0;
-            for (int j = 0; j < s2.length(); j++) {
-                int pos = i + j;
-                int a = s1.charAt(i) - '0';
-                int b = s2.charAt(j) - '0';
-                int sum = ret[pos] + a * b + carry;
-                ret[pos] = sum % 10;
-                carry = sum / 10;
-            }
-            if (carry != 0) {
-                ret[i + s2.length()] = carry;
-            }
+        if (n == 1) {
+            return x;
         }
-        return reverse(ret);
+        boolean isNegative = (n < 0);
+        n = Math.abs(n);
+        double ret = myPow(x, n >>> 1);
+        ret = ret * ret;
+        if ((n & 1) == 1) {
+            ret = ret * x;
+        }
+        return isNegative ? 1 / ret : ret;
     }
 
-    private static String reverse(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            sb.append(s.charAt(i));
-        }
-        return sb.toString();
-    }
-
-    private static String reverse(int[] arr) {
-        int end = arr.length - 1;
-        while (end >= 0 && arr[end] == 0) {
-            end--;
-        }
-        int l = 0, r = end;
-        while (l < r) {
-            int t = arr[l];
-            arr[l] = arr[r];
-            arr[r] = t;
-            l++;
-            r--;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= end; i++) {
-            sb.append((char)(arr[i] + '0'));
-        }
-        return sb.toString();
-    }
+//     public static double myPow(double x, int n) {
+//         if (n == 0) {
+//             return 1.0d;
+//         }
+//         if (n == -1) {
+//             return 1 / x;
+//         }
+//         if (n == 1) {
+//             return x;
+//         }
+//         boolean isNegative = false;
+//         if (n < 0) {
+//             n = Math.abs(n);
+//             isNegative = true;
+//         }
+//         double base = myPow(x, n/2);
+//         base = base * base;
+//         if ((n & 1) == 1) {
+//             base = base * x;
+//         }
+//         return isNegative ?  1 / base : base;
+//     }
 }
