5b1a570b33493fd8677f091686c6bc700f5b665a
August <18716558336@163.com>
2022-12-26 23:57:36 +0800

    all

--- src/Leetcode/TwoPointer/P15.java
+++ src/Leetcode/TwoPointer/P15.java
@@ -10,39 +10,37 @@ public class P15 {
         int[] nums = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> list = threeSum(nums);
         for (List<Integer> l : list) {
-            for (int k : l) {
-                System.out.print(k + " ");
-            }
-            System.out.println();
+            System.out.println(l);
         }
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> ret = new ArrayList<>();
         if (nums == null || nums.length < 3) {
-            return list;
+            return ret;
         }
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > 0) break;
-            help(list, nums, i + 1, nums.length - 1, -nums[i]);
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            twoSum(nums, -nums[i], i + 1, nums.length - 1, ret);
         }
-        return list;
+        return ret;
     }
 
-    public static void help(List<List<Integer>> list, int[] nums, int l, int r, int target) {
-        while (l < r) {
+    public static void twoSum(int[] nums, int target, int l, int r, List<List<Integer>> ret) {
+        if (nums == null || nums.length < 3 && target < 0 || l >= r) {
+            return;
+        }
+        while (l < r){
             int sum = nums[l] + nums[r];
             if (sum == target) {
-                List<Integer> ret = new ArrayList<>();
-                ret.add(-target);
-                ret.add(nums[l]);
-                ret.add(nums[r]);
-                list.add(ret);
+                ret.add(Arrays.asList(-target, nums[l], nums[r]));
                 while (l < r && nums[l] == nums[l + 1]) l++;
                 while (l < r && nums[r] == nums[r - 1]) r--;
-                l++; r--;
+                l++;
+                r--;
             } else if (sum > target) {
                 r--;
             } else {
@@ -50,4 +48,58 @@ public class P15 {
             }
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> list = new ArrayList<>();
+//        if (nums == null || nums.length < 3) {
+//            return list;
+//        }
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length; i++) {
+//            if (i > 0 && nums[i] == nums[i - 1]) continue;
+//            if (nums[i] > 0) break;
+//            help(list, nums, i + 1, nums.length - 1, -nums[i]);
+//        }
+//        return list;
+//    }
+//
+//    public static void help(List<List<Integer>> list, int[] nums, int l, int r, int target) {
+//        while (l < r) {
+//            int sum = nums[l] + nums[r];
+//            if (sum == target) {
+//                List<Integer> ret = new ArrayList<>();
+//                ret.add(-target);
+//                ret.add(nums[l]);
+//                ret.add(nums[r]);
+//                list.add(ret);
+//                while (l < r && nums[l] == nums[l + 1]) l++;
+//                while (l < r && nums[r] == nums[r - 1]) r--;
+//                l++; r--;
+//            } else if (sum > target) {
+//                r--;
+//            } else {
+//                l++;
+//            }
+//        }
+//    }
 }
--- src/Leetcode/TwoPointer/P16.java
+++ src/Leetcode/TwoPointer/P16.java
@@ -4,28 +4,32 @@ import java.util.Arrays;
 
 public class P16 {
 
-    public int threeSumClosest(int[] nums, int target) {
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int ret = Integer.MAX_VALUE;
-        int diff = Integer.MAX_VALUE;
+        int sum = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int a = nums[i];
             int l = i + 1, r = nums.length - 1;
             while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (Math.abs(sum - target) < diff) {
-                    ret = sum;
-                    diff = Math.abs(sum - target);
+                int currSum = a + nums[l] + nums[r];
+                if (Math.abs(currSum - target) < Math.abs(sum - target)) {
+                    sum = currSum;
                 }
-                if (sum == target) {
+                if (currSum == target) {
                     return target;
-                } else if (sum > target) {
-                    r--;
-                } else {
+                } else if (currSum < target) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }
-        return ret;
+        return sum;
     }
 }
